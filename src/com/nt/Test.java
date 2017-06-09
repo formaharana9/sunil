f7559f29918ca88b1cdd732f8af6dfd66e9c6af5
sunil <formaharana9@gmail.com>
2017-06-09 10:22:13 -0700

    changer done by user2

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("change done in master by user 2");
+		System.out.println("changes made in master by user1");
 	}
 
 }
