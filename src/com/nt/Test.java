ba5936e17e0971e02ae30d3385276aef0782608e
sunil <formaharana9@gmail.com>
2017-06-09 10:50:30 -0700

    minor change

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -3,7 +3,6 @@ package com.nt;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("change done in master by user 2");
 		System.out.println("changes made in master by user1");
