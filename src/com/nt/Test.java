e069ba4e1dd01b4c7fac71b63869f51898f5147a
sunil <formaharana9@gmail.com>
2017-06-13 21:32:52 -0700

    added new line

--- src/com/nt/Test.java
+++ src/com/nt/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("change done in master by user 2");
+		//kjflkjaslkfjaslkfjlaskjflkasjflkasjflkajslkfdjaslkjflkasjflkasjflkajslkfjalkj
+System.out.println("change done in master by user 2");
 		System.out.println("changes made in master by user1");
 	}
 
