a81c0c2e74d38f7004aa879f619d977d6b83ca0d
formaharana9 <formaharana9@gmail.com>
2018-10-15 22:35:20 +0530

    Update NewClass.java
    some changes done

--- src/com/nt/NewClass.java
+++ src/com/nt/NewClass.java
@@ -3,8 +3,8 @@ package com.nt;
 public class NewClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//sunil kumar maharana made the changes
+		//here is the code
 	}
 
 }
